bad3ba294964d8ca8a0461bdeec801fdabdb6d39
Pawan Kumar <pawan.kumar@gmail.com>
2020-04-05 10:41:20 +0000

    Run script

--- main.py
+++ main.py
@@ -23,4 +23,4 @@ def main(argv):
     pass
 
 if __name__ == '__main__':
-        main(sys.argv[1:])
+        main(sys.argv)
---
+++ run.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+
+nohup python main.py 0 100 &
+nohup python main.py 100 200 &
+nohup python main.py 200 300 &
+nohup python main.py 300 400 &
+nohup python main.py 400 500 &
+nohup python main.py 500 600 &
+nohup python main.py 600 700 &
+nohup python main.py 700 800 &
+nohup python main.py 800 900 &
+nohup python main.py 900 1000 &
