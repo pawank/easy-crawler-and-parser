17471c9e072b49246baecd9a787ff0f97f5893ad
Pawan Kumar <pawan@acelrtech.com>
2020-04-05 08:49:09 +0000

    Setup file updated

--- setup.sh
+++ setup.sh
@@ -3,5 +3,9 @@
 virtualenv --python=/usr/bin/python3 dev
 source dev/bin/activate
 pip3 install -r requirements.txt
+apt-get install google-chrome-stable
+mkdir drivers
+wget https://chromedriver.storage.googleapis.com/79.0.3945.36/chromedriver_linux64.zip
 export PATH="./drivers:$PATH"
+#nohup xvfb-run -a ./chromedriver &
 #python main.py
