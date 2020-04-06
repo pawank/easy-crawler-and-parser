4aacbb9a2b7dda5abe7f8748feb9acac47d07991
Pawan Kumar <pawan@acelrtech.com>
2020-04-06 11:06:28 +0530

    Created generate done from global data

--- main.py
+++ main.py
@@ -12,11 +12,25 @@ def test_s3_upload():
     #print(randomfile)
     upload_to_s3("projectstore", randomfile)
 
+def generate_done_list():
+    import simplejson as json
+    done_map = {}
+    with open('files/global_data.txt','r') as f:
+        lines = f.readlines()
+        for line in lines:
+                data = line.strip()
+                j = json.loads(data)
+                if "url" in j:
+                    done_map[j["url"]] = j["url"]
+    print('No of done urls = ', len(done_map.keys()))
+
+
 def main():
     #test_s3_upload()
     from crawler_parser import CrawlerParser
-    cp = CrawlerParser()
-    cp.run()
+    #cp = CrawlerParser()
+    #cp.run()
+    generate_done_list()
     pass
 
 if __name__ == '__main__':
